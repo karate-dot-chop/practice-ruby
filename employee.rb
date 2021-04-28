b55bf9982b42e433ceb8e933063b99b03d286904
dani <dani.zaghian@gmail.com>
2021-04-28 12:01:24 -0700

    adds employee as a lass

--- employee.rb
+++ employee.rb
@@ -5,12 +5,67 @@
 # puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
 
 # Represent an employee as a hash
-# string hashrocket
-employee1 = {"first_name" => "Jay", "last_name" => "Wengrow", "salary" => 1000000, "active" => true}
-# symbol hashrocket
-employee2 = {:first_name => "Peter", :last_name => "Jang", :salary => 20000, :active => true}
-# symbol javascript
-employee3 = {first_name: "Amanda", last_name: "Hale", salary: 30000, active: true}
-puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-puts "#{employee3[:first_name]} #{employee3[:last_name]} makes #{employee3[:salary]} a year."
+# # string hashrocket
+# employee1 = {"first_name" => "Jay", "last_name" => "Wengrow", "salary" => 1000000, "active" => true}
+# # symbol hashrocket
+# employee2 = {:first_name => "Peter", :last_name => "Jang", :salary => 20000, :active => true}
+# # symbol javascript
+# employee3 = {first_name: "Amanda", last_name: "Hale", salary: 30000, active: true}
+# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
+# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+# puts "#{employee3[:first_name]} #{employee3[:last_name]} makes #{employee3[:salary]} a year."
+
+# storeItem camel
+# store_item snake
+# store-item kebab
+# StoreItem Pascal
+
+# Represent an employee as a class instance
+class Employee
+
+  def initialize(first_name, last_name, salary, active)
+    @first_name = first_name
+    @last_name = last_name
+    @salary = salary
+    @active = active
+  end
+
+  def print_info
+    puts "#{first_name} #{last_name} makes #{@salary} a year."
+  end
+
+  # getter/reader
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def active
+    @active
+  end
+
+  # setter/writer
+  def active=(input_active)
+    @active = input_active
+  end
+
+  # instance method
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+
+end
+
+employee1 = Employee.new("Jay", "Wengrow", 1000000, true)
+employee2 = Employee.new("Peter", "Jang", 20000, true)
+# employee1.print_info
+# employee2.print_info
+puts employee1.first_name
+puts employee1.last_name
+puts employee1.active
+puts employee1.give_annual_raise
+employee1.active = false
+puts employee1.active
