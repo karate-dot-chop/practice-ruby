344f271c237a67fdcb862a685e0158706f9291a2
dani <dani.zaghian@gmail.com>
2021-04-29 12:30:46 -0700

    adds initializes with options hash

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,6 @@
 class Vehicle
-  def initialize
+  attr_accessor :speed, :direction
+  def initialize(*)
     @speed = 0
     @direction = 'north'
   end
@@ -18,18 +19,36 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_accessor :make, :model
+  def initialize(options_hash)
+    super
+    @make = options_hash[:make]
+    @model = options_hash[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  attr_accessor :weight, :color
+  def initialize(options_hash)
+    super
+    @weight = options_hash[:weight]
+    @color = options_hash[:color]
+  end
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
-bike = Bike.new
-car.honk_horn
-bike.ring_bell
+car = Car.new(make: "Ford", model: "Escape")
+bike = Bike.new(weight: 18, color: "Red")
+# car.honk_horn
+# bike.ring_bell
+puts car.model
+puts car.speed
+car.accelerate
+puts car.speed
+puts bike.weight
